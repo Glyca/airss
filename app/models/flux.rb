56c86d1389fc98d5b49d9b3d18d9040130e2a58f
DualModulus <dualmodulus@hotmail.com>
2015-05-11 12:00:48 +0200

    affichage contenu RSS

--- app/models/flux.rb
+++ app/models/flux.rb
@@ -1,11 +1,23 @@
+class Article
+  attr_accessor :title, :date, :description
+
+  def initialize(title, date, description)
+    @titre = title
+    @date = date
+    @description = description
+  end
+end
+
 class Flux < ActiveRecord::Base
 
   def items
     rss = SimpleRSS.parse open(self.url)
     coder = HTMLEntities.new
-    rss.items.each do |article|
-    article.description = coder.decode(article.description)
-    end
-    rss.items
+    articles = Array.new
+      rss.items.each {|article|
+        description = coder.decode(article.description.force_encoding('UTF-8'))
+        articles.push Article.new(article.title.force_encoding('UTF-8'),article.pubDate, description)
+        }
+    articles
   end
 end
