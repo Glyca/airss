25f6f47b4be1f77a70f1f282e1f0a16ea86ae8f7
DualModulus <dualmodulus@hotmail.com>
2015-05-25 15:54:45 +0200

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "localhost:3000",
+    domain: ENV["GMAIL_DOMAIN"],
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
