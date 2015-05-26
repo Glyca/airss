11ffbfb7521fd22453c5b2bf1bf06bf5de17dd87
Glyca <glycaa@gmail.com>
2015-05-26 15:38:37 +0200

    Ajout du hub

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,11 @@
 class WelcomeController < ApplicationController
 
+  respond_to :html
+
   # GET /welcome
   def index
-
+    @articles = Article.order(views: :desc).limit(9)
+    respond_with(@articles)
   end
 
 end
